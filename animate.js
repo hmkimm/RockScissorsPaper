3fc43217b1221b2afe065ece59bba02b44bed3bc
hmkimm <lisamin7@naver.com>
2023-05-06 01:37:44 +0900

    코드 간소화

--- animate.js
+++ animate.js
@@ -1,87 +1,81 @@
 const emojis = ["✌️", "✊", "🖐"]; // 순서대로 이모지 배열
+const $btn = document.querySelector("button");
 
 let i = 0; // 현재 이모지의 인덱스
 let timerId = 0; // setTimeout의 리턴값 저장용 변수
 
 function animateEmoji() {
-  let randomEmoji = Math.floor(Math.random(emojis) * 3);
-  document.getElementById("emoji").textContent = emojis[randomEmoji]; // 현재 이모지 출력
+  document.getElementById("emoji").innerText = emojis[i]; // 현재 이모지 출력
+  // i = parseInt(Math.random() * 3); //이렇게 하면 이모지 넘어갈 때 약간의 딜레이 발생
   i = (i + 1) % 3;
-  timerId = setTimeout(animateEmoji, 1000); // 0.2초마다 이모지 변경
+  timerId = setTimeout(animateEmoji, 200); // 0.2초마다 이모지 변경
 }
 
 animateEmoji(); // 애니메이션 시작
 
-// 게임 결과
+// 게임 결과 변수
 const $span = document.querySelector("span");
 const $labels = document.querySelectorAll("label");
-// let rsp = document.querySelector("p").innerText;
-// rsp = "✌️";
-let result = document.querySelector("p");
+const computerResult = document.querySelector("p");
 
-const rspGame = () => {
-  $labels.forEach((el) => {
-    el.addEventListener("click", (event) => {
-      clearTimeout(timerId);
+// 게임 결과 구현
+$labels.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    clearTimeout(timerId);
+    const clickResult = e.target.textContent;
 
-      const game = event.target.textContent;
+    // 내가 가위 냈을 때
+    if (clickResult == "가위") {
+      switch (computerResult.innerText) {
+        case "🖐":
+          $span.innerHTML = "이겼어요 🎉";
+          break;
 
-      //내가 가위 냈을 때
-      if (game == "가위") {
-        switch (result.innerText) {
-          case "🖐":
-            $span.innerHTML = "이겼어요 🎉";
-            break;
+        case "✊":
+          $span.innerHTML = "졌어요 😞";
 
-          case "✊":
-            $span.innerHTML = "졌어요 😞";
-            break;
+          break;
 
-          default:
-            $span.innerHTML = "비겼어요 다시 한 번!";
-            animateEmoji();
-        }
-      } else if (game == "바위") {
-        switch (result.innerText) {
-          case "🖐":
-            $span.innerHTML = "졌어요 😞";
-            break;
-
-          case "✊":
-            $span.innerHTML = "비겼어요 다시 한 번!";
-            animateEmoji();
-            break;
-
-          default:
-            $span.innerHTML = "이겼어요 🎉";
-        }
-      } else if (game == "보") {
-        switch (result.innerText) {
-          case "🖐":
-            $span.innerHTML = "비겼어요 다시 한 번!";
-            animateEmoji();
-            break;
-
-          case "✊":
-            $span.innerHTML = "이겼어요 🎉";
-            break;
-
-          default:
-            $span.innerHTML = "졌어요😞";
-            break;
-        }
+        default:
+          $span.innerHTML = "비겼어요 다시 한 번!";
+          animateEmoji();
       }
-    });
+    } else if (clickResult == "바위") {
+      switch (computerResult.innerText) {
+        case "🖐":
+          $span.innerHTML = "졌어요 😞";
+          break;
+
+        case "✊":
+          $span.innerHTML = "비겼어요 다시 한 번!";
+          animateEmoji();
+          break;
+
+        default:
+          $span.innerHTML = "이겼어요 🎉";
+      }
+    } else if (clickResult == "보") {
+      switch (computerResult.innerText) {
+        case "🖐":
+          $span.innerHTML = "비겼어요 다시 한 번!";
+          animateEmoji();
+          break;
+
+        case "✊":
+          $span.innerHTML = "이겼어요 🎉";
+          break;
+
+        default:
+          $span.innerHTML = "졌어요😞";
+          break;
+      }
+    }
   });
-};
-rspGame();
+});
 
 const restart = () => {
-  const $btn = document.querySelector("button");
-  $btn.addEventListener("click", animateEmoji);
-  $btn.addEventListener("click", () => {
-    $span.innerText = "결과는?";
-  });
+  animateEmoji();
+  $span.innerText = "결과는?";
 };
 
-restart();
+$btn.addEventListener("click", restart);
